c426f840fe115279e7e29782828045bb09732e9b
Your Name <mohamed1_1_1994@hotmail.com>
2020-11-22 02:17:41 -0800

    Databse problem fixed

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::resource('user', 'UserController');
+
+Route::resource('permission', 'PermissionController');
+
+Route::resource('role', 'RoleController');
+
+
+
+Route::get('/profile', 'UserController@profile')->name('user.profile');
+
+Route::post('/profile', 'UserController@postProfile')->name('user.postProfile');
+
+Route::get('/password/change', 'UserController@getPassword')->name('userGetPassword');
+
+Route::post('/password/change', 'UserController@postPassword')->name('userPostPassword');
+
+
+
+
+Auth::routes();
+
+
+//////////////////////////////// axios request
+
+Route::get('/getAllPermission', 'PermissionController@getAllPermissions');
+Route::post("/postRole", "RoleController@store");
